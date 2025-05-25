be8e454c0066b84f676702f7cec881f35c54cef6
Daniel <daniielmayo.mayo@gmail.com>
2025-05-25 14:20:57 -0500

    serverless

--- app/app.js
+++ app/app.js
@@ -10,13 +10,22 @@ const db_1 = __importDefault(require("./config/db"));
 const user_routes_1 = __importDefault(require("./routes/user.routes"));
 const songs_routes_1 = __importDefault(require("./routes/songs.routes"));
 const swagger_1 = require("./docs/swagger");
+const serverless_http_1 = __importDefault(require("serverless-http"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
+const corsOptions = {
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200,
+    "Access-Control-Allow-Origin": "*",
+};
+app.use((0, cors_1.default)(corsOptions));
 app.use(express_1.default.json());
 (0, db_1.default)();
 (0, swagger_1.setupSwagger)(app);
+app.use(express_1.default.static("public"));
 app.use("/api", user_routes_1.default);
 app.use("/api", songs_routes_1.default);
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
+// app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
+module.exports.handler = (0, serverless_http_1.default)(app);
