a9f26027752b9f2983a8ea21c58aea7a0c315ae2
Daniel <daniielmayo.mayo@gmail.com>
2025-05-25 14:00:42 -0500

    cors options

--- app/app.ts
+++ app/app.ts
@@ -8,15 +8,21 @@ import { setupSwagger } from "./docs/swagger";
 
 dotenv.config();
 const app = express();
+const corsOptions = {
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200,
+    "Access-Control-Allow-Origin": "*",
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 connectDB();
 setupSwagger(app);
 app.use(express.static("public"));
 app.use("/api", userRoutes);
-app.use("/api",songRoutes);
+app.use("/api", songRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
